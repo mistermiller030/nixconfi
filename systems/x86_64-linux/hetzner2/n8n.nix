f06e08bb34778139706963522b4094283c593df4
david <david.mueller.bln@gmail.com>
2025-04-10 16:40:41 +0300

    remove acme

--- systems/x86_64-linux/hetzner2/n8n.nix
+++ systems/x86_64-linux/hetzner2/n8n.nix
@@ -8,8 +8,7 @@
   services.nginx = {
     enable = true;
     virtualHosts = {
-      "n8n.david.ritter.family" = {
-        useACMEHost = "n8n.david.ritter.family";
+      "n8n.a24-umzuege.de" = {
         locations."/" = {
           proxyPass = "http://127.0.0.1:5678";
           recommendedProxySettings = true;
@@ -18,17 +17,6 @@
     };
   };
 
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "beneritter@gmail.com";
-    certs."n8n.david.ritter.family" = {
-      # An acme system user is created. This user belongs to the acme group and the home directory is /var/lib/acme.
-      # This user will try to make the directory .well-known/acme-challenge/ under the webroot directory.
-      webroot = "/var/lib/acme";
-    };
-  };
-
-  users.users.nginx.extraGroups = ["acme"];
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
