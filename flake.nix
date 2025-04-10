dd59a730144ecd51d17ad63f67359ef94b926be0
david <david.mueller.bln@gmail.com>
2025-04-10 12:00:37 +0300

    enable comin

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
+    comin = {
+      url = "github:nlewo/comin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,21 +21,24 @@
       nixpkgs,
       home-manager,
       disko,
+      comin,
     }:
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./systems/x86_64-linux/nixos/configuration.nix
           home-manager.nixosModules.home-manager
+          ./systems/x86_64-linux/nixos/configuration.nix
         ];
       };
       nixosConfigurations.hetzner1 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
+          comin.nixosModules.comin
           ./systems/x86_64-linux/hetzner1/configuration.nix
         ];
+
       };
 
     };
