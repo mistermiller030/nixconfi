57784d22a812d3cf90b3d87f50973c279db269cd
david <david.mueller.bln@gmail.com>
2025-04-10 16:05:43 +0300

    configure ssl

--- systems/x86_64-linux/hetzner2/n8n.nix
+++ systems/x86_64-linux/hetzner2/n8n.nix
@@ -9,6 +9,14 @@
     enable = true;
     virtualHosts = {
       "n8n.david.ritter.family" = {
+        onlySSL = true;
+        useACMEHost = "n8n.david.ritter.family";
+        locations."/.well-known/acme-challenge/" = {
+          extraConfig = ''
+            rewrite /.well-known/acme-challenge/(.*) /$1 break;
+            root /var/lib/acme/.well-known/acme-challenge;
+          '';
+        };
         locations."/" = {
           proxyPass = "http://127.0.0.1:5678";
           recommendedProxySettings = true;
@@ -16,6 +24,19 @@
       };
     };
   };
-  networking.firewall.allowedTCPPorts = [ 80 ];
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "admin@example.org";
+    certs."n8n.david.ritter.family" = {
+      # An acme system user is created. This user belongs to the acme group and the home directory is /var/lib/acme.
+      # This user will try to make the directory .well-known/acme-challenge/ under the webroot directory.
+      webroot = "/var/lib/acme";
+    };
+  };
+
+  users.users.nginx.extraGroups = ["acme"];
+
+  networking.firewall.allowedTCPPorts = [ 443 ];
 
 }
