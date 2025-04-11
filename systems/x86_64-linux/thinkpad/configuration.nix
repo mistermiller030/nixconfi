1fc1acfa4275d42eea351034557b695edbe57b77
david <david.mueller.bln@gmail.com>
2025-04-11 16:19:44 +0300

    new configuration for thinkpad

--- systems/x86_64-linux/thinkpad/configuration.nix
+++ systems/x86_64-linux/thinkpad/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports =
@@ -6,19 +6,49 @@
       ./hardware-configuration.nix
     ];
 
-  # Boot-Loader
+  # Use the systemd-boot EFI boot loader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # ThinkPad-spezifische Einstellungen
+  # Allow unfree packages (necessary for ThinkPad firmware)
+  nixpkgs.config.allowUnfree = true;
+
+  # Network configuration
+  networking.hostName = "thinkpad"; # Set your hostname
+  networking.networkmanager.enable = true;  # Use NetworkManager for Wi-Fi
+
+  # Set your time zone
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties
+  i18n.defaultLocale = "de_DE.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "de";
+  };
+
+  # Enable the X11 windowing system
+  services.xserver.enable = true;
+  
+  # Configure keymap in X11
+  services.xserver.layout = "de";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable GNOME Desktop Environment
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # ThinkPad-specific settings
   hardware.enableAllFirmware = true;
   hardware.cpu.intel.updateMicrocode = true;
-  
-  # Für TrackPoint
   hardware.trackpoint.enable = true;
   hardware.trackpoint.emulateWheel = true;
 
-  # Energiemanagement
+  # Energy management for better battery life
   services.thermald.enable = true;
   services.tlp.enable = true;
   services.tlp.settings = {
@@ -26,44 +56,43 @@
     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
   };
 
-  # Netzwerk
-  networking.hostName = "thinkpad";
-  networking.networkmanager.enable = true;
+  # Enable touchpad support
+  services.xserver.libinput.enable = true;
 
-  # Benutzer
-  users.users.david = {  # Ersetze "david" mit deinem Benutzernamen
+  # Define your user account
+  users.users.david = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
-    shell = pkgs.zsh;
+    initialPassword = "changeme";  # Remember to change this after first login
   };
 
-  # Home Manager - die gleiche Konfiguration wie dein Hauptrechner
+  # Enable ZSH
+  programs.zsh.enable = true;
+  users.users.david.shell = pkgs.zsh;
+
+  # Home Manager configuration
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.david = {  # Ersetze "david" mit deinem Benutzernamen
+    users.david = {
       home.stateVersion = "24.11";
-      imports = [ ../../x86_64-linux/nixos/home.nix ];  # Verwendet deine bestehende home.nix
+      # We'll set this up after the initial installation
     };
-    backupFileExtension = "backup";
   };
 
-  # System-Pakete speziell für den Laptop
+  # Install basic system packages
   environment.systemPackages = with pkgs; [
-    acpi
-    powertop
+    vim
     git
-    neovim
     wget
+    firefox
+    gnome.gnome-terminal
     eza
     bat
+    acpi
+    powertop
   ];
 
-  # Zeitzone und Sprache
-  time.timeZone = "Europe/Berlin";
-  i18n.defaultLocale = "de_DE.UTF-8";
-  
-  # Weitere Einstellungen kannst du nach Bedarf hinzufügen
-
-  system.stateVersion = "24.11";
+  # This value determines the NixOS release with which your system is to be compatible
+  system.stateVersion = "24.11"; # DO NOT CHANGE
 }
